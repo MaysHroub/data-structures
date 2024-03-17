6160a79fd690efb34dfdf10ab568f2d29be2e5ca
Mays-Alreem <hroubmays@gmail.com>
2024-03-17 23:09:50 +0200

    Create a method to insert the nodes.

--- DataStructures/src/linkedlist/LinkedList.java
+++ DataStructures/src/linkedlist/LinkedList.java
@@ -4,4 +4,40 @@ public class LinkedList<T extends Comparable<T>> {
 	
 	Node<T> head;
 	
+	
+	public void insert(T data) {
+		Node<T> newNode = new Node<>(data);
+		
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+		
+		Node<T> prev = null, curr = head;
+		for (; curr != null && curr.data.compareTo(data) < 0;
+				prev = curr, curr = curr.next);
+		
+		if (prev == null /*or curr == head*/) {  // insert first
+			newNode.next = head;
+			head = newNode;
+		}
+		else if (curr == null)   // insert last
+			prev.next = newNode;
+		else {                   // insert between
+			newNode.next = curr;
+			prev.next = newNode;
+		}
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
