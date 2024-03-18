65cc39f929209dc184b685019846186f0e19b807
Mays-Alreem <hroubmays@gmail.com>
2024-03-19 00:53:04 +0200

    Add insert method

--- DataStructures/src/linkedlist/DoublyLinkedList.java
+++ DataStructures/src/linkedlist/DoublyLinkedList.java
@@ -4,4 +4,37 @@ public class DoublyLinkedList<T extends Comparable<T>> {
 	
 	DNode<T> head;
 
+	
+	public void insert(T data) {
+		DNode<T> newNode = new DNode<>(data);
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+		
+		DNode<T> curr = head;
+		for (; curr.next != null && curr.data.compareTo(data) < 0;
+				curr = curr.next);
+		
+		if (curr == head) { // insert first
+			newNode.next = head;
+			head.prev = newNode;
+			head = newNode;
+		}
+		else if (curr.next == null) { // insert last
+			newNode.prev = curr;
+			curr.next = newNode;
+		}
+		else { // insert between
+			newNode.next = curr;
+			newNode.prev = curr.prev;
+			curr.prev.next = newNode;
+			curr.prev = newNode;
+		}
+	}
+	
 }
+
+
+
+
