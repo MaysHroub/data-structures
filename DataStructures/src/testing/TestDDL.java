0503869a6896363ce4137cbfdbe624e40ec19c61
Mays-Alreem <hroubmays@gmail.com>
2024-03-19 08:59:22 +0200

    Comment out original methods to test other recursive methods

--- DataStructures/src/testing/TestDDL.java
+++ DataStructures/src/testing/TestDDL.java
@@ -35,26 +35,26 @@ public class TestDDL {
 		dll.traverse();
 
 		// -------------- delete existing data
-		dll.delete(40);
-		dll.traverse();
-		dll.delete(99);
-		dll.traverse();
-		dll.delete(60);
-		dll.traverse();
-		// -------------- delete non-existing data
-		dll.delete(44);
-		dll.delete(101);
-		dll.delete(57);
-		dll.traverse();
+//		dll.delete(40);
+//		dll.traverse();
+//		dll.delete(99);
+//		dll.traverse();
+//		dll.delete(60);
+//		dll.traverse();
+//		// -------------- delete non-existing data
+//		dll.delete(44);
+//		dll.delete(101);
+//		dll.delete(57);
+//		dll.traverse();
 
 		// -------------- search for existing data
-		System.out.println(dll.find(50));
-		System.out.println(dll.find(90));
-		System.out.println(dll.find(55));
-		// -------------- search for non-existing data
-		System.out.println(dll.find(40));
-		System.out.println(dll.find(99));
-		System.out.println(dll.find(60));
+//		System.out.println(dll.find(50));
+//		System.out.println(dll.find(90));
+//		System.out.println(dll.find(55));
+//		// -------------- search for non-existing data
+//		System.out.println(dll.find(40));
+//		System.out.println(dll.find(99));
+//		System.out.println(dll.find(60));
 	}
 
 }
