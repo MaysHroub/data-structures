4f25d47f34e020b953c8954ef401c47236312a2c
Mays-Alreem <hroubmays@gmail.com>
2024-03-19 08:17:56 +0200

    Test find method

--- DataStructures/src/testing/TestDDL.java
+++ DataStructures/src/testing/TestDDL.java
@@ -7,6 +7,9 @@ public class TestDDL {
 	public static void main(String[] args) {
 		DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
 		
+		dll.delete(29);
+		System.out.println(dll.find(55));
+		
 		// -------------- insert new data
 		dll.insert(50);
 		dll.traverse();
@@ -33,6 +36,12 @@ public class TestDDL {
 		dll.delete(57);
 		dll.traverse();
 		
+		System.out.println(dll.find(50));
+		System.out.println(dll.find(90));
+		System.out.println(dll.find(55));
+		System.out.println(dll.find(40));
+		System.out.println(dll.find(99));
+		System.out.println(dll.find(60));
 	}
 
 }
