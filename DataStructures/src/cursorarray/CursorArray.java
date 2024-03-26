7b50194d08c0390a321826611608ba71cd17a717
Mays-Alreem <hroubmays@gmail.com>
2024-03-26 23:23:19 +0200

    Add init method to initialise the cursor array

--- DataStructures/src/cursorarray/CursorArray.java
+++ DataStructures/src/cursorarray/CursorArray.java
@@ -8,5 +8,11 @@ public class CursorArray<T extends Comparable<T>> {
 		arr = new CNode[size];
 	}
 	
+	private void init() {
+		for (int i = 0; i < arr.length - 1; i++)
+			arr[i] = new CNode<>(null, i + 1);
+		arr[arr.length - 1] = new CNode<>(null, 0);
+	}
+	
 	
 }
