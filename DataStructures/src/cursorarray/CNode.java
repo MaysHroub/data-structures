0695b7124a7300976a2033c51913f5e05febe019
Mays-Alreem <hroubmays@gmail.com>
2024-03-26 23:18:39 +0200

    Create node class for the cursor array

---
+++ DataStructures/src/cursorarray/CNode.java
@@ -0,0 +1,17 @@
+package cursorarray;
+
+public class CNode<T extends Comparable<T>> {
+	
+	T data;
+	int next;
+	
+	public CNode(T data) {
+		this.data = data;
+	}
+	
+	public CNode(T data, int next) {
+		this.data = data;
+		this.next = next;
+	}
+	
+}
