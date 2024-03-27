e26791abcd8e0930801ef36f26179010e285be87
Mays-Alreem <hroubmays@gmail.com>
2024-03-28 00:12:39 +0200

    test recursive methods

--- DataStructures/src/testing/TestCursorArr.java
+++ DataStructures/src/testing/TestCursorArr.java
@@ -1,10 +1,39 @@
 package testing;
 
+import cursorarray.CursorArray;
+
 public class TestCursorArr {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CursorArray<Integer> cursorArray = new CursorArray<>(11);
+		cursorArray.traverse(0);
+		
+		int ml = cursorArray.createList();
+		cursorArray.insertAtHead(1, ml);
+		cursorArray.recursiveInsertAtTail(5, ml);
+		cursorArray.recursiveInsertAtTail(10, ml);
+		cursorArray.recursiveInsertAtTail(15, ml);
+		
+		cursorArray.recursiveTraverse(ml);
+		
+		System.out.println(cursorArray.recursiveLength(ml));
+		
+		System.out.println(cursorArray.recursiveDeleteFromTail(ml));
+		
+		cursorArray.recursiveTraverse(ml);
+		
+		System.out.println(cursorArray.recursiveFind(5, ml));
+		System.out.println(cursorArray.find(5, ml));
+		
+		System.out.println(cursorArray.findPrevious(10, ml));
+		System.out.println(cursorArray.recursiveFindPrevious(10, ml));
+		
+		System.out.println(cursorArray.length(ml));
+		System.out.println(cursorArray.recursiveLength(ml));
+		
+		System.out.println(cursorArray.recursiveLength(0));
+		cursorArray.recursiveRemoveList(ml);
+		System.out.println(cursorArray.recursiveLength(0));
 	}
 
 }
