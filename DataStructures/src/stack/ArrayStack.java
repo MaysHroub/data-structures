22d93ec4f37dccb8503cf59e92ae155a37c73131
Mays-Alreem <hroubmays@gmail.com>
2024-03-28 23:56:26 +0200

    add stackable methods

--- DataStructures/src/stack/ArrayStack.java
+++ DataStructures/src/stack/ArrayStack.java
@@ -1,5 +1,35 @@
 package stack;
 
-public class ArrayStack<T> {
+public class ArrayStack<T> implements Stackable<T>{
+
+	@Override
+	public void push(T data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
