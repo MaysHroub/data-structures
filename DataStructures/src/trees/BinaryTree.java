1f54fbc4985ba62a26a8b11319d3d1d443bc06c7
Mays-Alreem <hroubmays@gmail.com>
2024-04-17 00:00:00 +0300

    add in-order traverse method

--- DataStructures/src/trees/BinaryTree.java
+++ DataStructures/src/trees/BinaryTree.java
@@ -4,6 +4,15 @@ public class BinaryTree<T extends Comparable<T>> {
 	
 	private TNode<T> root;
 	
-	
+	public void traverseInOrder() {
+		traverseInOrder(root);
+	}
+
+	private void traverseInOrder(TNode<T> curr) {
+		if (curr == null) return;
+		if (curr.hasLeft()) traverseInOrder(curr.getLeft());
+		System.out.println(curr + " ");
+		if (curr.hasRight()) traverseInOrder(curr.getRight());
+	}
 
 }
