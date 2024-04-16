7c8455a73d118c4ed6acddf4890599359249b614
Mays-Alreem <hroubmays@gmail.com>
2024-04-16 10:16:56 +0300

    create a queue class using a stack

---
+++ DataStructures/src/queue_pack/SQueue.java
@@ -0,0 +1,43 @@
+package queue_pack;
+
+import stack.LinkedListStack;
+
+public class SQueue<T extends Comparable<T>> implements Queueable<T>{
+
+	private LinkedListStack<T> items, temp;
+	
+	public SQueue() {
+		items = new LinkedListStack<>();
+		temp = new LinkedListStack<>();
+	}
+	
+	@Override
+	public void enqueue(T data) {
+		while (!items.isEmpty()) 
+			temp.push(items.pop());
+		items.push(data);
+		while (!temp.isEmpty()) 
+			items.push(temp.pop());
+	}
+
+	@Override
+	public T dequeue() {
+		return items.pop();
+	}
+
+	@Override
+	public T getFront() {
+		return items.peek();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return items.isEmpty();
+	}
+
+	@Override
+	public void clear() {
+		items.clear();
+	}
+
+}
