b4a98be48fd400091dc60a4e1975f31f78a4b0bc
Mays-Alreem <hroubmays@gmail.com>
2024-05-04 00:12:58 +0300

    update linked list

--- DataStructures/src/linkedlist/LinkedList.java
+++ DataStructures/src/linkedlist/LinkedList.java
@@ -1,9 +1,9 @@
 package linkedlist;
 
+
 public class LinkedList<T extends Comparable<T>> {
 
-	Node<T> head;
-	
+	private Node<T> head;
 	
 	public void insert(T data) {
 		Node<T> newNode = new Node<>(data);
@@ -14,133 +14,141 @@ public class LinkedList<T extends Comparable<T>> {
 		}
 		
 		Node<T> prev = null, curr = head;
-		for (; curr != null && curr.data.compareTo(data) < 0;
-				prev = curr, curr = curr.next);
+		for (; curr != null && curr.getData().compareTo(data) < 0;
+				prev = curr, curr = curr.getNext());
 		
 		if (prev == null /*or curr == head*/) {  // insert first
-			newNode.next = head;
+			newNode.setNext(head);
 			head = newNode;
 		}
 		else if (curr == null)   // insert last
-			prev.next = newNode;
-		
+			prev.setNext(newNode);
 		else {                   // insert between
-			newNode.next = curr;
-			prev.next = newNode;
+			newNode.setNext(curr);
+			prev.setNext(newNode);
 		}
 	}
-	
 	
 	public void recursiveInsert(T data) {
 		recursiveInsert(data, null, head);
 	}
 	
 	private void recursiveInsert(T data, Node<T> prev, Node<T> curr) {
-		if (curr == null || curr.data.compareTo(data) >= 0) {
+		if (curr == null || curr.getData().compareTo(data) >= 0) {
 			Node<T> newNode = new Node<>(data);
 			if (prev == null /*or curr == head*/) {  // insert first
-				newNode.next = head;
+				newNode.setNext(head);
 				head = newNode;
 			}
 			else if (curr == null)   // insert last
-				prev.next = newNode;
+				prev.setNext(newNode);
 			else {                   // insert between
-				newNode.next = curr;
-				prev.next = newNode;
+				newNode.setNext(curr);
+				prev.setNext(newNode);
 			}
 		}
 		else
-			recursiveInsert(data, curr, curr.next);
+			recursiveInsert(data, curr, curr.getNext());
 	}
-	
 	
 	public Node<T> find(T data) {
 		Node<T> curr = head;
-		
-		for (; curr != null && curr.data.compareTo(data) < 0;
-				curr = curr.next);
-		
-		if (curr != null && curr.data.compareTo(data) == 0)
+		for (; curr != null && curr.getData().compareTo(data) < 0;
+				curr = curr.getNext());
+		if (curr != null && curr.getData().compareTo(data) == 0)
 			return curr;
-		
 		return null;
 	}
-	
 	
 	public Node<T> recursiveFind(T data) {
 		return recursiveFind(data, head);
 	}
 	
 	private Node<T> recursiveFind(T data, Node<T> curr) {
-		if (curr == null || curr.data.compareTo(data) > 0) 
+		if (curr == null || curr.getData().compareTo(data) > 0) 
 			return null;
-		if (curr != null && curr.data.compareTo(data) == 0)
+		if (curr != null && curr.getData().compareTo(data) == 0)
 			return curr;
-		return recursiveFind(data, curr.next);
+		return recursiveFind(data, curr.getNext());
 	}
-	
 	
 	public Node<T> delete(T data) {
 		Node<T> prev = null, curr = head;
-		
-		for (; curr != null && curr.data.compareTo(data) < 0;
-				prev = curr, curr = curr.next);
-		
-		if (curr != null && curr.data.compareTo(data) == 0) {
+		for (; curr != null && curr.getData().compareTo(data) < 0;
+				prev = curr, curr = curr.getNext());
+		if (curr != null && curr.getData().compareTo(data) == 0) {
 			if (prev == null)  // delete the first node
-				head = curr.next;
-			else if (curr.next == null) // delete the last node
-				prev.next = null;
+				head = curr.getNext();
+			else if (curr.getNext() == null) // delete the last node
+				prev.setNext(null);
 			else                // delete a middle node
-				prev.next = curr.next;
+				prev.setNext(curr.getNext());
 			return curr;
 		}
 		return null;
 	}
-	
 	
 	public Node<T> recursiveDelete(T data) {
 		return recursiveDelete(data, null, head);
 	}
 	
 	private Node<T> recursiveDelete(T data, Node<T> prev, Node<T> curr) {
-		if (curr == null || curr.data.compareTo(data) > 0)
+		if (curr == null || curr.getData().compareTo(data) > 0)
 			return null;
-		
-		if (curr != null && curr.data.compareTo(data) == 0) {
+		if (curr != null && curr.getData().compareTo(data) == 0) {
 			if (prev == null)  // delete the first node
-				head = curr.next;
-			else if (curr.next == null) // delete the last node
-				prev.next = null;
+				head = curr.getNext();
+			else if (curr.getNext() == null) // delete the last node
+				prev.setNext(null);
 			else                // delete a middle node
-				prev.next = curr.next;
+				prev.setNext(curr.getNext());
 			return curr;
 		}
-		return recursiveDelete(data, curr, curr.next);
+		return recursiveDelete(data, curr, curr.getNext());
 	}
-	
 	
 	public int length() {
 		int count = 0;
 		Node<T> curr = head;
 		while (curr != null) {
 			count++;
-			curr = curr.next;
+			curr = curr.getNext();
 		}
 		return count;
 	}
 	
+	public void reverse() {
+		Node<T> curr = head;
+		while (curr != null && curr.getNext() != null) {
+			Node<T> temp = curr.getNext();
+			curr.setNext(curr.getNext().getNext());
+			temp.setNext(head);
+			head = temp;
+		}
+	}
+	
+	public void recursiveReverse() {
+		recursiveReverse(head);
+	}
+	
+	private void recursiveReverse(Node<T> curr) {
+		if (curr == null || curr.getNext() == null) return;
+		Node<T> temp = curr.getNext();
+		curr.setNext(curr.getNext().getNext());
+		temp.setNext(head);
+		head = temp;
+		recursiveReverse(curr);
+	}
 	
 	public void traverse() {
 		Node<T> curr = head;
 		System.out.print("Head --> ");
 		while (curr != null) {
 			System.out.print(curr + " --> ");
-			curr = curr.next;
+			curr = curr.getNext();
 		}
 		System.out.println("Null");
 	}
-	
 	
 	@Override
 	public String toString() {
@@ -148,7 +156,7 @@ public class LinkedList<T extends Comparable<T>> {
 		String linkedlist = "Head --> ";
 		while (curr != null) {
 			linkedlist += curr + " --> ";
-			curr = curr.next;
+			curr = curr.getNext();
 		}
 		return linkedlist + "Null";
 	}
--- DataStructures/src/linkedlist/Node.java
+++ DataStructures/src/linkedlist/Node.java
@@ -2,16 +2,32 @@ package linkedlist;
 
 public class Node<T extends Comparable<T>> {
 	
-	public T data;
-	public Node<T> next;
+	private T data;
+	private Node<T> next;
 	
 	public Node(T data) {
-		this.data = data;
+		this.setData(data);
 	}
 	
 	@Override
 	public String toString() {
 		return data.toString();
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
 	}
 	
 }
