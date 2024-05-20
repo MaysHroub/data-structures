ffdd346976c60118e0226f772d90ec785dfc1143
Mays-Alreem <hroubmays@gmail.com>
2024-05-20 20:18:20 +0300

    add sink method

--- DataStructures/src/heap/MaxHeap.java
+++ DataStructures/src/heap/MaxHeap.java
@@ -58,4 +58,14 @@ public class MaxHeap<T extends Comparable<T>> implements MaxHeapInterface<T> {
 		arr[c] = temp;
 	}
 	
+	private void sink(int k) {
+		while (2*k <= N) {
+			int j = 2*k;
+			if (j < N && less(j, j+1)) j++;
+			if (!less(k, j)) break;
+			swap(k, j);
+			k = j;
+		}
+	}
+	
 }
