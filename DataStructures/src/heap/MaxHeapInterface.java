2a344f58a614904e334409196f42b8a1e4a04e4c
Mays-Alreem <hroubmays@gmail.com>
2024-05-20 20:04:04 +0300

    add max-heap methods

--- DataStructures/src/heap/MaxHeapInterface.java
+++ DataStructures/src/heap/MaxHeapInterface.java
@@ -1,5 +1,12 @@
 package heap;
 
 public interface MaxHeapInterface<T extends Comparable<T>> {
-
+	
+	void add(T data);
+	T removeMax();
+	T getMax();
+	boolean isEmpty();
+	int getSize();
+	void clear();
+	
 }
