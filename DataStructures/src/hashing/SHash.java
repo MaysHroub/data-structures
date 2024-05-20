199f1d17e10cd5cdec27241053af734968e5330a
Mays-Alreem <hroubmays@gmail.com>
2024-05-20 17:36:21 +0300

    create a hash class handling collisions using separate-chaining

---
+++ DataStructures/src/hashing/SHash.java
@@ -0,0 +1,48 @@
+package hashing;
+
+import linkedlist.LinkedList;
+import linkedlist.Node;
+
+public class SHash<T extends Comparable<T>> {
+	
+	private LinkedList<T>[] table;
+	int size;
+	
+	@SuppressWarnings("unchecked")
+	public SHash(int size) {
+		this.size = size;
+		table = new LinkedList[size];
+		for (int i = 0; i < size; i++) 
+			table[i] = new LinkedList<>();
+	}
+	
+	public void add(T data) {
+		int index = Math.abs(data.hashCode()) % size;
+		table[index].insertSorted(data);
+	}
+	
+	public Node<T> delete(T data) {
+		int index = Math.abs(data.hashCode()) % size;
+		return table[index].delete(data);
+	}
+	
+	public Node<T> find(T data) {
+		int index = Math.abs(data.hashCode()) % size;
+		return table[index].find(data);
+	}
+	
+	public void printLinkedList(int index) {
+		if (index >= size) 
+			System.out.println("Invalid index");
+		else 
+			table[index].traverse();
+	}
+	
+	public void printAllLinkedLists() {
+		for (int i = 0; i < size; i++) {
+			System.out.print(i + " - ");
+			table[i].traverse();
+		}
+	}
+	
+}
