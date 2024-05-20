e4fd90a56e101451e22ef674598f1957d43a7763
Mays-Alreem <hroubmays@gmail.com>
2024-05-20 20:25:05 +0300

    add unimplemented methods

--- DataStructures/src/heap/MinHeap.java
+++ DataStructures/src/heap/MinHeap.java
@@ -2,4 +2,40 @@ package heap;
 
 public class MinHeap implements MinHeapInterface<T> {
 
+	@Override
+	public void add(T data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T removeMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getMin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
