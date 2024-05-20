38dae630a85fff200071bc7c264fd37e064362fb
Mays-Alreem <hroubmays@gmail.com>
2024-05-20 20:23:53 +0300

    add methods

--- DataStructures/src/heap/MinHeapInterface.java
+++ DataStructures/src/heap/MinHeapInterface.java
@@ -1,5 +1,12 @@
 package heap;
 
 public interface MinHeapInterface<T extends Comparable<T>> {
+	
+	void add(T data);
+	T removeMin();
+	T getMin();
+	boolean isEmpty();
+	int getSize();
+	void clear();
 
 }
