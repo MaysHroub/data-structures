1c20b2b15b83da30da32c5c64568ce008479597f
Mays-Alreem <hroubmays@gmail.com>
2024-05-20 20:05:01 +0300

    implement max-heap-interface

--- DataStructures/src/heap/MaxHeap.java
+++ DataStructures/src/heap/MaxHeap.java
@@ -1,5 +1,41 @@
 package heap;
 
-public class MaxHeap<T extends Comparable<T>> {
+public class MaxHeap<T extends Comparable<T>> implements MaxHeapInterface<T> {
+
+	@Override
+	public void add(T data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T removeMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getMax() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
