a6cfc4be43adf1a5c5064498cce5a6f2e2055844
Mays-Alreem <hroubmays@gmail.com>
2024-05-28 09:20:35 +0300

    test heapify method

--- DataStructures/src/testing/TestHeap.java
+++ DataStructures/src/testing/TestHeap.java
@@ -1,9 +1,19 @@
 package testing;
 
+import heap.MaxHeap;
+
 public class TestHeap {
 
 	public static void main(String[] args) {
-
+		Integer[] a = {0, 55, 60, 70, 80, 20};
+		// 55 80 70 60 20
+		// 80 55 70 60 20
+		// 80 60 70 55 20
+		MaxHeap.heapify(a);
+		for (int i = 1; i < a.length; i++)
+			System.out.print(a[i] + " ");
 	}
+	
+	
 
 }
