1a6bf91f2c8c29607da4980b190d45d5a37135c8
Mays-Alreem <hroubmays@gmail.com>
2024-05-28 22:21:45 +0300

    test heapify methods

--- DataStructures/src/testing/TestHeap.java
+++ DataStructures/src/testing/TestHeap.java
@@ -6,8 +6,18 @@ import heap.MinHeap;
 public class TestHeap {
 
 	public static void main(String[] args) {
-		
-		
+
+		Integer[] a = { 0, 55, 60, 70, 80, 20 };
+
+		MaxHeap.maxHeapify(a);
+		for (int i = 1; i < a.length; i++)
+			System.out.print(a[i] + " ");
+
+		System.out.println();
+
+		MinHeap.minHeapify(a);
+		for (int i = 1; i < a.length; i++)
+			System.out.print(a[i] + " ");
 
 	}
 
