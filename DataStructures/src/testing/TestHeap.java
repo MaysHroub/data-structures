97e390f14ae87f418de5dd289c59f9af75cf3c71
Mays-Alreem <hroubmays@gmail.com>
2024-05-28 22:25:49 +0300

    test is-max/min-heap methods

--- DataStructures/src/testing/TestHeap.java
+++ DataStructures/src/testing/TestHeap.java
@@ -9,15 +9,24 @@ public class TestHeap {
 
 		Integer[] a = { 0, 55, 60, 70, 80, 20 };
 
+		System.out.println("MaxHeap? " + MaxHeap.isMaxHeap(a));
+		System.out.println("MinHeap? " + MinHeap.isMinHeap(a));
+		
 		MaxHeap.maxHeapify(a);
 		for (int i = 1; i < a.length; i++)
 			System.out.print(a[i] + " ");
-
 		System.out.println();
+		
+		System.out.println("MaxHeap? " + MaxHeap.isMaxHeap(a));
+		System.out.println("MinHeap? " + MinHeap.isMinHeap(a));
 
 		MinHeap.minHeapify(a);
 		for (int i = 1; i < a.length; i++)
 			System.out.print(a[i] + " ");
+		System.out.println();
+		
+		System.out.println("MaxHeap? " + MaxHeap.isMaxHeap(a));
+		System.out.println("MinHeap? " + MinHeap.isMinHeap(a));
 
 	}
 
