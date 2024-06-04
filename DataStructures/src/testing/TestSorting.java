9552273cd9dbae5e25141080dbaaea113f67298c
Mays-Alreem <hroubmays@gmail.com>
2024-06-04 09:19:36 +0300

    implement insertion-sort method

--- DataStructures/src/testing/TestSorting.java
+++ DataStructures/src/testing/TestSorting.java
@@ -101,5 +101,17 @@ public class TestSorting {
 			h /= 3;
 		}
 	}
+	
+	public static void insertionSort(Comparable[] a) {
+		int N = a.length;
+		for (int i = 0; i < N; i++)
+			for (int j = i; j > 0; j--)
+				if (a[j].compareTo(a[j-1]) < 0) {
+					Comparable temp = a[j];
+					a[j] = a[j-1];
+					a[j-1] = temp;
+				}
+				else break;
+	}
 
 }
