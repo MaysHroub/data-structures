e47b4a6fa9ca0dc3c010b18f2fca6929ee4ed8c8
Mays-Alreem <hroubmays@gmail.com>
2024-06-04 09:29:31 +0300

    remove age field

--- DataStructures/src/testing/Martyr.java
+++ DataStructures/src/testing/Martyr.java
@@ -4,25 +4,14 @@ public class Martyr implements Comparable<Martyr> {
 	
 	private String name, district, location;
 	private char gender;
-	private int age;
 	
 	public Martyr() {}
 	
-	public Martyr(String name, String district, String location, char gender, int age) {
+	public Martyr(String name, String district, String location, char gender) {
 		setName(name);
 		setDistrict(district);
 		setLocation(location);
 		setGender(gender);
-		setAge(age);
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		if (age < 0) throw new IllegalArgumentException("Invalid age value");
-		this.age = age;
 	}
 
 	public char getGender() {
@@ -68,52 +57,6 @@ public class Martyr implements Comparable<Martyr> {
 	@Override
 	public String toString() {
 		return name;
-	}
-	
-	public static void heapSortAsc(Martyr[] a) {
-		int N = a.length - 1;
-		Martyr temp;
-		
-		maxHeapify(a);  // to a max-heap
-		
-		while (N > 1) {
-			// 1. Swap the first element with the last element
-			temp = a[1];
-			a[1] = a[N];
-			a[N] = temp;
-			
-			// 2. decrement N
-			N--;
-			
-			// 3. sink[1]
-			int k = 1;
-			while (2*k <= N) {
-				int j = 2*k;
-				if (j < N && a[j].age < a[j+1].age) j++;
-				if (a[k].age >= a[j].age) break;
-				temp = a[k];
-				a[k] = a[j];
-				a[j] = temp;
-				k = j;
-			}
-		}
-	}
-	
-	public static void maxHeapify(Martyr[] a) {
-		int N = a.length - 1, i = N / 2;
-		Martyr temp;
-		while (i-- > 0) {
-			int k = i+1;
-			while (2*k <= N) {
-				int j = 2*k;
-				if (j < N && a[j].compareTo(a[j+1]) < 0) j++;
-				if (a[k].age >= a[j].age) break;
-				temp = a[k];
-				a[k] = a[j];
-				a[j] = temp;
-				k = j;
-			}
-		}
 	}
 
 }
