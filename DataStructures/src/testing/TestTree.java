00825356ef569668da1d98adfadc7d9547a0f2f6
Mays-Alreem <hroubmays@gmail.com>
2024-09-18 00:16:08 +0300

    test bst and avl trees

--- DataStructures/src/testing/TestTree.java
+++ DataStructures/src/testing/TestTree.java
@@ -1,11 +1,39 @@
 package testing;
 
+import trees.AVLTree;
+import trees.BinarySearchTree;
 
 public class TestTree {
-
+	
 	public static void main(String[] args) {
-		// this is now your playground :)
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+		bst.insert(5);
+		bst.insert(3);
+		bst.insert(7);
+		bst.traverseLevelOrder();
+		
+		AVLTree<Integer> avl = new AVLTree<>();
+		avl.insert(1);
+		avl.delete(1);
+		avl.traverseInOrder();
 	}
 	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
